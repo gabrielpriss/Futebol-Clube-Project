bab142c35b553e0ea9731de31834028d5908aa24
gabrielpriss <gabrielpriss@hotmail.com>
2022-06-27 18:18:43 -0300

    created tests for route 'Teams'

---
+++ app/backend/src/database/interfaces/aaa.js
@@ -0,0 +1,10 @@
+const oi
+
+function check(a) {
+    if (a === false) {
+        console.log('falso')
+    }
+    else {
+        console.log('oi')
+    }
+}
---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,37 @@
+import sinon from 'sinon';
+import chai from 'chai';
+// @ts-ignore
+import chaiHttp from 'chai-http';
+import teams from './mocks/teams';
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Route', () => {
+  let chaiHttpResponse: Response;
+
+  describe('/api/teams', () => {
+    it('should return status 200 with all teams', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .get('/teams')
+  
+      expect(chaiHttpResponse.status).to.be.eql(200);
+      expect(chaiHttpResponse.body).to.be.deep.equal(teams);
+    });
+
+        it('should return status 200 with a specific team on path /:id', async () => {
+        chaiHttpResponse = await chai
+            .request(app)
+            .get('/teams/1')
+    
+        expect(chaiHttpResponse.status).to.be.eql(200);
+        expect(chaiHttpResponse.body).to.be.deep.equal(teams[0]);
+        });
+    })
+});
